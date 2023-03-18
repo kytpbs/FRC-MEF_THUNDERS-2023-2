0fa71b3252d9681c09eadd034dae05b324b1cbc1
kytpbs <95276965+kytpbs@users.noreply.github.com>
2023-03-18 13:48:35 +0300

    Changed Limit switches so they work now

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -12,40 +12,42 @@ public class Teleop {
     }
 
     public static void elevator() {
-        if (stick.getPOV() == 0) {
-            if (toplimitSwitch.get()){
-              Elevator.set(-1);
-            }
-          }
-          else {
-            if (stick.getPOV() == 180) {
-              if (bottomlimitSwitch.get()){
-                Elevator.set(1);
-              }
-            }
-            else {
-              Elevator.set(0);
-            }
-          }  
+      if (stick.getPOV() == 0) {
+        if (toplimitSwitch.get()) {
+          Elevator.set(0.8);
+        }
+      }
+      if (stick.getPOV() == 180) {
+        if (bottomlimitSwitch.get()) {
+          Elevator.set(-0.8);
+        }
+      }
+      if (stick.getPOV() == -1) {
+        Elevator.set(0);
+      }
+
+      if (!bottomlimitSwitch.get()) {
+        if (Elevator.get() != 0 && Elevator.get() < 0) {
+          Elevator.set(0);
+        }
+      }
+      if (!toplimitSwitch.get()) {
+        if (Elevator.get() != 0 && Elevator.get() > 0) {
+          Elevator.set(0);
+        }
+      }
+
         if (stick.getRawButtonPressed(1)) {
-            Virtical_Elevator.set(0.5);
+            Virtical_Elevator.set(0.8);
         }
         if (stick.getRawButtonReleased(1)) {
             Virtical_Elevator.set(0);
         }
         if (stick.getRawButtonPressed(2)) {
-            Virtical_Elevator.set(-0.5);
+            Virtical_Elevator.set(-0.8);
         }
         if (stick.getRawButtonReleased(2)) {
             Virtical_Elevator.set(0);
         }
-        
-        
-        if (!bottomlimitSwitch.get()) {
-            Elevator.set(0);
-        }
-          if (!toplimitSwitch.get()) {
-            Elevator.set(0);
-          }
     }
 }
